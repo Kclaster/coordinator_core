266a568ff76da298d33e26ca8cd0b19701dc7266
Kclaster <kyle.laster@gmail.com>
2020-05-28 21:45:06 -0500

    comment out configuration

--- src/main/java/com/coordinator/core/cofig/DatabaseConfiguration.java
+++ src/main/java/com/coordinator/core/cofig/DatabaseConfiguration.java
@@ -1,37 +1,30 @@
 package com.coordinator.core.cofig;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfiguration {
-    @Value("${DB_PASSWORD}")
-    private String dbPasowrd;
-
-    @Value("${DB_USERNAME}")
-    private String dbUsername;
-
-    @Value("${DB_URL}")
-    private String dbUrl;
-
-    @Bean
-    @ConfigurationProperties(prefix = "datasource.core")
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create().create();
-        builder.url(dbUrl);
-        builder.password(dbPasowrd);
-        builder.username(dbUsername);
-        return builder.build();
-    }
-
-    @Bean
-    public JdbcTemplate coreJdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
+//    @Value("${DB_PASSWORD}")
+//    private String dbPasowrd;
+//
+//    @Value("${DB_USERNAME}")
+//    private String dbUsername;
+//
+//    @Value("${DB_URL}")
+//    private String dbUrl;
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "datasource.core")
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create().create();
+//        builder.url(dbUrl);
+//        builder.password(dbPasowrd);
+//        builder.username(dbUsername);
+//        return builder.build();
+//    }
+//
+//    @Bean
+//    public JdbcTemplate coreJdbcTemplate() {
+//        return new JdbcTemplate(dataSource());
+//    }
 }
