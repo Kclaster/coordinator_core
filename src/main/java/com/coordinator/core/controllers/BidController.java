72dd30fe8e8d77553da2f5d96c012934b8ef3355
Kclaster <kyle.laster@gmail.com>
2020-06-05 21:19:15 -0500

    change requestBody to requestParameters

--- src/main/java/com/coordinator/core/controllers/BidController.java
+++ src/main/java/com/coordinator/core/controllers/BidController.java
@@ -1,14 +1,16 @@
 package com.coordinator.core.controllers;
 
 import com.coordinator.core.models.BidDto;
-import com.coordinator.core.models.QueryOptions;
 import com.coordinator.core.services.IBid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+
+import static com.coordinator.core.utils.QueryOptionsHelper.fromQueryParameters;
 
 @RestController
 @RequestMapping("/api/v1/coordinators/{coordinatorId}/bids")
@@ -18,7 +20,10 @@ public class BidController {
 
     @GetMapping
     public ResponseEntity<List<BidDto>> getAllCoordinatorsBids(@PathVariable(value = "coordinatorId") String coordinatorId,
-                                                               @RequestBody final QueryOptions queryOptions) {
-        return ResponseEntity.ok(iBid.getAllCoordinatorsBids(UUID.fromString(coordinatorId), queryOptions));
+                                                               @RequestParam Map<String, String> queryOptions) {
+        return ResponseEntity.ok(iBid.getAllCoordinatorsBids(
+                UUID.fromString(coordinatorId),
+                fromQueryParameters(queryOptions)
+        ));
     }
 }
