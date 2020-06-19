e8ebc990011fce4c9b2611f7e207500f24cb5ca5
Kclaster <kyle.laster@gmail.com>
2020-06-19 18:26:53 -0500

    expetion handling

--- src/main/java/com/coordinator/core/models/CoordinatorPostRequest.java
+++ src/main/java/com/coordinator/core/models/CoordinatorPostRequest.java
@@ -1,6 +1,11 @@
 package com.coordinator.core.models;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class CoordinatorPostRequest {
+    @NotNull
+    @NotEmpty
     private String username;
 
     public String getUsername() {
